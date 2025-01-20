f06c1a7f9a760c44be8a946a1165e9516146ba07
mikailakey <mikaila.keyzha7@smk.belajar.id>
2025-01-20 20:36:58 +0700

    menambahkan proses kategori update

--- IdeKreatif-starter-main/proses_kategori.php
+++ IdeKreatif-starter-main/proses_kategori.php
@@ -33,15 +33,15 @@ if(isset($_POST['simpan'])){
     exit();
 }
 
-//
-
-    }
-
-    if (isset($_POST['delete'])){
+//Proses penghapusan kategori
+if (isset($_POST['delete'])){
+    //Mengambil ID kategori dari parameter URL
         $catID = $_POST['catID'];
 
+        //Query untuk menghapus kategori berdasarkan ID 
         $exec = mysqli_query($conn, "DELETE FROM categories WHERE category_id='$_catID'");
 
+        //Menyimpan notifikasi keberhasilan atau kegagalan ke dalam session
         if ($exec){
             $_SESSION['notification'] = [
                 'type' => 'primary',
@@ -54,6 +54,37 @@ if(isset($_POST['simpan'])){
             ];
         }
 
+        //Redirect kembali ke halaman kategori
         header('Location: kategori.php');
         exit();
     }
+
+   //Proses pembaruan kategori
+   if (isset($_POST['update'])) {
+    //Mengambil data dari form pembaruan
+    $catID = $_POST['catID'];
+    $category_name = $_POST['category_name'];
+
+    //Query untuk memperbarui dat kategori berdasarkan ID 
+    $query = "UPDATE categories SET category_name = '$category_name' WHERE category_id= '$catID'";
+    $exec = mysqli_query($conn, $query);
+
+    //Menyimpan notifikasi keberhasilan atau kegagalan ke dalam session
+    if ($exec) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Kategori berhasil di perbarui!'
+        ];
+    } else {
+        $_SESSION['notification'] =[
+            'type' => 'danger',
+            'message' => 'Gagal memperbarui kategori: ' . mysqli_error($conn)
+        ];
+    }
+
+    //Redirect kembali ke halaman kategori
+    header('Location: kategori.php');
+    exit();
+}
+
+       
